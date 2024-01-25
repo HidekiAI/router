d8ade5bcc54561d30b0fca62ce879deae527902b
Hideki A. Ikeda <hidekiai+hidekiai-lt@CodeMonkeyNinja.dev@users.noreply.github.com>
2024-01-25 12:50:42 -0600

    Began work on switching over to rust (GDExtension) rather than using GDScript

---
+++ rust/block_units/src/lib.rs
@@ -0,0 +1,21 @@
+use godot::prelude::*;
+
+struct MyExtension;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
